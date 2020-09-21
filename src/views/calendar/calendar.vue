63674ccb589a4d792cbb086fd3bbec083243a325
ydydydq <yd@suncard.vip>
2020-09-21 11:16:12 +0800

    update: 优化日期组件逻辑

--- src/views/calendar/calendar.vue
+++ src/views/calendar/calendar.vue
@@ -2,11 +2,11 @@
   <div>
     <div class="werp">
       <header>
-        <span class="date-left">{{num + "号"}} {{day}}</span>
+        <span class="date-left">{{new Date().getDate() + "号"}} {{week}}</span>
         <div class="date-right">
-          <span class="btn-left" @click="handlePrevMonth()">&lt;</span>
-          <span>{{year}} {{month+1 + "月"}}</span>
-          <span class="btn-right">&gt;</span>
+          <span class="btn-left" @click.stop="chooseMonth(dateData.month-1)">&lt;</span>
+          <span>{{dateData.year}} {{dateData.month + "月"}}</span>
+          <span class="btn-right" @click.stop="chooseMonth(dateData.month+1)">&gt;</span>
         </div>
       </header>
       <div class="main">
@@ -15,10 +15,10 @@
         </ul>
         <ul class="day">
           <li
-            v-for="(item,index) in refreshDate"
+            v-for="(item, index) in dateData.days"
             :key="index"
-            :class="[{now:index==nowDayIndex},{ismonthDay:isMonth(item.date.getMonth())}]"
-          >{{item.day}}</li>
+            :class="{'is-now': item.isCurrentDay === 0, 'is-not-cur-month': item.showMonth !== dateData.month}"
+          >{{item.showDay}}</li>
         </ul>
       </div>
       <footer></footer>
@@ -28,41 +28,24 @@
 
 <script>
 export default {
-  name: "",
+  name: "calendar",
   data() {
     return {
-      month_run: [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31],
-      month_normal: [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31],
-      weeks: ["一", "二", "三", "四", "五", "六", "日"],
-      months: [
-        "一月",
-        "二月",
-        "三月",
-        "四月",
-        "五月",
-        "六月",
-        "七月",
-        "八月",
-        "九月",
-        "十月",
-        "十一月",
-        "十二月",
-      ],
-      date: new Date(),
+      dateData: {
+        year: new Date().getFullYear(),
+        month: new Date().getMonth()+1,
+        days: [],
+      },
+      weeks: ["日", "一", "二", "三", "四", "五", "六"],
     };
   },
+  created() {
+    this.dateData = this.getMonthAllDay();
+  },
   computed: {
-    //获取当前年份
-    year: function () {
-      return this.date.getFullYear();
-    },
-    //获取当前月份
-    month: function () {
-      return this.date.getMonth();
-    },
-    //获取当前星期数
-    day: function () {
-      switch (this.date.getDay()) {
+    // 获取当前星期数
+    week() {
+      switch (new Date().getDay()) {
         case 0:
           return "星期天";
           break;
@@ -85,96 +68,119 @@ export default {
           return "星期六";
       }
       return;
-    },
-    //获取今天的号数
-    num: function () {
-      return this.date.getDate();
-    },
-    //获取这个月的的天数
-    monthDay: function () {
-      return this.daysMonth(this.month, this.year);
-    },
-    //获取第一天是星期几
-    Startday: function () {
-      return this.dayStart(this.month, this.year);
-    },
-    refreshDate: function () {
-      //获取到的日期数组存到这里
-      let calendatArr = [];
-      //获取当月的第一天
-      let utils = new Date(this.year, this.month, 1);
-      //用当月的第一天减去 周几前面几天 这样就能得到上个月开始的天数 （当前月1号是周三，那么周一就是上个月的最后两天）
-      let startTime = utils - (this.Startday - 1) * 24 * 60 * 60 * 1000;
-      //然后得到所有的日期
-      for (let i = 0; i < 35; i++) {
-        calendatArr.push({
-          date: new Date(startTime + i * 24 * 60 * 60 * 1000),
-          year: this.year,
-          month: this.month + 1,
-          day: new Date(startTime + i * 24 * 60 * 60 * 1000).getDate(),
-        });
-      }
-      return calendatArr;
-    },
-    //获取当月1号的索引值，来判断当天的索引值
-    nowDayIndex: function () {
-      let nowDayIndex = this.my_day.indexOf(1);
-      return this.num + nowDayIndex - 1;
-    },
-
-    //把日期存进数组
-    my_day: function () {
-      let my_days = [];
-      for (let i = 0; i < 35; i++) {
-        my_days.push(this.refreshDate[i].day);
-      }
-      return my_days;
-    },
+    }
   },
   methods: {
-    //获取数组里的月份
-    isMonth: function (month) {
-      if (month == this.month) {
-        return false;
+    // 获取每个月的天数，42天为一整体
+    getMonthAllDay(year, month){
+      let monthData = [];
+      // 初始默认值
+      if (!year || !month) {
+        year = new Date().getFullYear();
+        month = new Date().getMonth() + 1;
       }
-      return true;
+
+      // 获取本月第一天和这天是星期几
+      let curMonthFirstDay = new Date(year, month - 1, 1);
+      let curMonthFirstDayWeek = curMonthFirstDay.getDay() === 0 ? 7 : curMonthFirstDay.getDay();
+      year = curMonthFirstDay.getFullYear();
+      month = curMonthFirstDay.getMonth() + 1;
+
+      // 获取本月最后一天和这天是几号
+      let curMonthLastDay = new Date(year, month, 0);
+      let curMonthLastDayNum = curMonthLastDay.getDate();
+
+      // 获取上个月最后一天和这天是几号
+      let prevMonthLastDay = new Date(year, month - 1, 0); // month - 1:为原来的月份,0:可以变成一个月最后一天
+      let prevMonthLastDayNum = prevMonthLastDay.getDate();
+
+      // 计算补多少天为上个月的
+      let prevMonthDayCount = curMonthFirstDayWeek - 1; // 设计本月1号为周四,则4-1 = 3,还要补三天
+
+      for (let i = 0; i < 7 * 6; i++) {
+        let date = i - prevMonthDayCount;
+        let showDay = date;
+        let showMonth = month;
+        let showYear = year;
+
+        // 天数越界处理
+        if (date <= 0) {
+          // 上一个月
+          showDay = prevMonthLastDayNum + date;
+          showMonth = month - 1;
+        }
+        else if (date > curMonthLastDayNum) {
+          // 下个月
+          showDay = showDay - curMonthLastDayNum;
+          showMonth = month + 1;
+        }
+
+        // 月数越界处理
+        if (showMonth <= 0) showMonth = 12;
+        if (showMonth > 12) showMonth = 1;
+
+        // 年数越界处理
+        if (month === 12 && showMonth === 1) showYear = year + 1;
+        if (month === 1 && showMonth === 12) showYear = year - 1;
+
+        monthData.push({
+          showYear,
+          showMonth,
+          showDay,
+          date,
+          isCurrentDay: this.compareDate(showYear + '/' + showMonth + '/' + showDay),
+          isSaturday: new Date(showYear + '/' + showMonth + '/' + showDay).getDay() === 6 ? 1 : 0,
+          isSunday: new Date(showYear + '/' + showMonth + '/' + showDay).getDay() === 0 ? 1 : 0
+        })
+      }
+      return {
+        year,
+        month,
+        days: monthData
+      }
     },
-    //返回这个月第一天是星期几
-    dayStart: function (month, year) {
-      let tmpDate = new Date(year, month, 1);
-      return tmpDate.getDay();
-    },
-    //返回这个月的天数
-    daysMonth: function (month, year) {
-      let Monthdays = 0;
-      if (year % 4 == 0) {
-        Monthdays = this.month_run[month];
-        return Monthdays;
+    // 比较两个日期大小
+    compareDate(date1, date2 = new Date(), compareTime = false) {
+      let flag = null;
+      if (!date1) {
+        console.warn('请输入比较的日期');
+        return;
+      }
+      if (!(date1 instanceof Date)) {
+        date1 = new Date(date1.replace(/-/g, "\/"));
+      }
+      if (!(date2 instanceof Date)) {
+        date2 = new Date(date2.replace(/-/g, "\/"));
+      }
+      // 不比较时间,只比较日期
+      if (!compareTime) {
+        date1 = new Date(date1.getFullYear() + '/' + (date1.getMonth() + 1) + '/' + date1.getDate());
+        date2 = new Date(date2.getFullYear() + '/' + (date2.getMonth() + 1) + '/' + date2.getDate());
+      }
+      if (date1.getTime() < date2.getTime()) {
+        flag = -1;
+      } else if (date1.getTime() == date2.getTime()) {
+        flag = 0;
       } else {
-        Monthdays = this.month_normal[month];
-        return Monthdays;
+        flag = 1;
       }
+      return flag;
     },
-    // 上一个月
-    handlePrevMonth() {
-      let utils = new Date(this.year, this.month, 1);
-      // let prevMonth = .getDate(this.year, this.month, 1);
-      this.date.setMonth(utils.getMonth() - 1);
-      this.month = 8;
-      console.log(this.date.getMonth());
-      // this.month = this.date.getmonth(prevMonth);
-      console.log(this.date);
-    },
-  },
-  mounted() {
-    console.log(this.date);
+    // 选择月份
+    chooseMonth(month) {
+      if(month < 1) {
+        this.dateData.year--;
+        month = 1;
+      }else if(month > 12) {
+        this.dateData.year++;
+        month = 12;
+      }
+      this.dateData.month = month;
+      this.dateData = this.getMonthAllDay(this.dateData.year, this.dateData.month);
+    }
   },
 };
 </script>
-
-
-
-
 
 <style lang='less' scoped>
 * {
@@ -212,7 +218,6 @@ header > span,
 .date-right {
   padding: 0 40px;
 }
-
 .weekday {
   display: flex;
   justify-content: space-between;
@@ -230,6 +235,8 @@ header > span,
   padding: 0 55px;
   flex-wrap: wrap;
   text-align: center;
+  box-sizing: border-box;
+  padding-bottom: 10px;
 }
 .day li {
   width: 50px;
@@ -237,13 +244,12 @@ header > span,
   line-height: 50px;
   margin: 3px 0;
 }
-
-.now {
+.is-now {
   border-radius: 50px;
   background-color: blue;
   color: white;
 }
-.ismonthDay {
+.is-not-cur-month {
   color: #cccccc;
 }
 </style>
